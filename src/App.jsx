198be3e1e5233cbd485c82099216c3f31b0be527
clispy <84087311+clispy1@users.noreply.github.com>
2023-01-03 09:19:51 +0000

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./style";
 
 const App = () => (
 	<div className="bg-primary w-full underline overflow-hidden text-white">
-		Hello, World!
+		<div className={`${style.paddingX}`}></div>
 	</div>
 );
 
